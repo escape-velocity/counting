ff09409d60831c871a7787f004c6f5c498459696
Bill <networker747@gmail.com>
2017-05-03 23:01:32 +0000

    create the value positions script

--- key-value-mapping.js
+++ key-value-mapping.js
@@ -1,1 +1,21 @@
-key_value_mapping.js
+
+
+
+
+function keyValueMapping(str) {
+  var output = {};
+  var char = str.split('')
+
+ for (var i = 0; i < char.length; i++) {
+  if (char[i] in output) {
+     output[char[i]].push(i);
+   }
+   else {
+    output[char[i]] = [i];
+    }
+  }
+ return output;
+}
+
+console.log(keyValueMapping('Lighthouse in the house'));
+
